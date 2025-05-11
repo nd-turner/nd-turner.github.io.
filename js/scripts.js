5fbfdd868cc320b2452ccf499c68dc354c85bc9e
Nathan Turner <ndturner@mail.bradley.edu>
2025-05-11 10:47:11 -0500

    Fixed padding issue with h2 skills header

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,5 @@
-/*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scriptwindow.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    }
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Add onclick functionality for "Experience" and "Education" selectors
+window.addEventListener('DOMContentLoaded', event => {
+    // Button click handlers
     const experienceSelector = document.getElementById('ExperienceSelector');
     const educationSelector = document.getElementById('EducationSelector');
 
@@ -42,9 +13,10 @@
         });
     }
 
-    // Function to change content in the Experience_Education_Information container
     function changeContent(type) {
         const contentContainer = document.getElementById('contentContainer');
+
+        if (!contentContainer) return;
 
         if (type === 'Experience') {
             contentContainer.innerHTML = `
@@ -106,4 +78,7 @@
             `;
         }
     }
+
+    // Optional: Load Experience by default
+    changeContent('Experience');
 });
